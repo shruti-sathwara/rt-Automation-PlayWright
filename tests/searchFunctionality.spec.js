1d5afcb6697bbb46e408eef6a081b6d594091e60
shruti-sathwara <59195275+shruti-sathwara@users.noreply.github.com>
2024-01-12 00:09:19 +0530

    Update searchFunctionality.spec.js

--- tests/searchFunctionality.spec.js
+++ tests/searchFunctionality.spec.js
@@ -1,22 +1,21 @@
 const { test, expect } = require('@playwright/test');
 const { chromium } = require('playwright');
 
-// Seach Functionality
-test('Seach Functionality', async ({ page }) => {
-    // Launch a new browser instance
+// Search Functionality
+test('Search Functionality', async ({ page }) => {
+   
  const browser = await chromium.launch();
-
-   // Navigate to the Amazon login page
    await page.goto('https://www.amazon.in');
 
-   // Locate and interact with the login button
+
    await page.type('#twotabsearchtextbox','Redmi 13C');
    await page.keyboard.press('Enter');
 
-const isLoggedIn = await page.getByRole('link', { name: 'Redmi 13C (Stardust Black, 4GB RAM, 128GB Storage) | 90Hz Display | 50MP AI' }).nth(2);
 
-await expect(isLoggedIn).toBeVisible();
-await expect(isLoggedIn).toContainText("Redmi 13C");
+const selector = 'span:has-text("Redmi 13C (Starshine Green, 4GB RAM, 128GB Storage) | Powered by 4G MediaTek Helio G85 | 90Hz Display | 50MP AI Triple Camera")';
+await page.waitForSelector(selector);
+const element = await page.$(selector);
+const elementText = await element.evaluate(el => el.textContent);
 
   await browser.close();
 });
